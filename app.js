4af66f411210b3102e19f6e9cf393b078efaef2c
Pablo <98pablofl@gmail.com>
2022-09-20 20:35:50 -0300

    commit

--- app.js
+++ app.js
@@ -1,124 +1,13 @@
-let condicionRepetir = true;
-let pagoConTarjeta = false;
+const buttonJava = document.querySelector("#bttJava");
+const buttonFull = document.querySelector("#bttFullStack");
+const bttPython = document.querySelector("#bttPython");
 
-class Compra {
-  constructor(nombreSolucion, precio, moneda, metodoPago, descuento) {
-    this.nombreSolucion = nombreSolucion;
-    this.precioBase = precio;
-    this.moneda = moneda;
-    this.metodoPago = metodoPago;
-    this.descuento = descuento;
-  }
-}
+const cursoComprado = document.querySelector("#cursoComprado");
 
-do {
-  let numCurso = 0;
-  numCurso = prompt(
-    "¡Bienvenido a Soluciones Informaticas!\n \n" +
-      "Seleccione la solución informatica a adquirir: \n \n" +
-      "1)Software de finanzas (1000 USD) \n" +
-      "2)ECommerce (3000 USD) \n" +
-      "3)Portal de noticias (15000 UYU)"
-  );
+buttonJava.onclick = () => { cursoComprado.innerHTML = "<h2>COMPRASTE EL CURSO <FONT COLOR='red'>JAVA</FONT></h2> <img src='https://cdn-icons-png.flaticon.com/512/226/226777.png' width='80px' height='80px'>" }
 
-  switch (parseInt(numCurso)) {
-    case 1:
-      pagoConTarjeta = usaTarjeta(pagoConTarjeta);
-      var com = new Compra(
-        "software de finanzas",
-        1000,
-        "USD",
-        pagoConTarjeta,
-        descuento10(1000, pagoConTarjeta)
-      );
-      alert(
-        "Gracias por tu compra de " +
-          com.nombreSolucion +
-          ", el valor es de: " +
-          (parseInt(com.precioBase) -
-            parseInt(com.descuento) +
-            " " +
-            com.moneda)
-      );
+buttonFull.onclick = () => { cursoComprado.innerHTML = "<h2>COMPRASTE EL CURSO  <FONT COLOR='blue'>FULL STACK </FONT></h2> <img src='https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,f_auto,q_auto:eco,dpr_1/v1468442513/ntffgniiqfya5tvzbsol.png' width='80px' height='80px'>" }
 
-      condicionRepetir = false;
-      break;
+bttPython.onclick = () => { cursoComprado.innerHTML = "<h2>COMPRASTE EL CURSO <FONT COLOR='yellow'>PYT</FONT><FONT COLOR='blue'>HON</FONT></h2> <img src='https://seeklogo.com/images/P/python-logo-C50EED1930-seeklogo.com.png' width='80px' height='80px'>" }
 
-    case 2:
-      pagoConTarjeta = usaTarjeta(pagoConTarjeta);
-      var com = new Compra(
-        "eCommerce",
-        3000,
-        "UYU",
-        pagoConTarjeta,
-        descuento10(3000, pagoConTarjeta)
-      );
-      alert(
-        "Gracias por tu compra de " +
-          com.nombreSolucion +
-          ", el valor es de: " +
-          (parseInt(com.precioBase) -
-            parseInt(com.descuento) +
-            " " +
-            com.moneda)
-      );
 
-      condicionRepetir = false;
-      break;
-
-    case 3:
-      pagoConTarjeta = usaTarjeta(pagoConTarjeta);
-      var com = new Compra(
-        "portal de noticias",
-        15000,
-        "UYU",
-        pagoConTarjeta,
-        descuento10(15000, pagoConTarjeta)
-      );
-      alert(
-        "Gracias por tu compra de " +
-          com.nombreSolucion +
-          ", el valor es de: " +
-          (parseInt(com.precioBase) -
-            parseInt(com.descuento) +
-            " " +
-            com.moneda)
-      );
-
-      condicionRepetir = false;
-      break;
-
-    default:
-      var answer = window.confirm(
-        "Ninguna opción corresponde al valor ingresado. ¿Desea ingresar otro valor?"
-      );
-      if (!answer) {
-        alert("Adios!");
-        condicionRepetir = false;
-      }
-  }
-} while (condicionRepetir);
-
-function usaTarjeta(pagoConTarjeta) {
-  var answer = window.confirm(
-    "¿Desea pagar con credito? (Se aplicara un descuento adicional del 10%)"
-  );
-
-  if (answer) {
-    pagoConTarjeta = true;
-    return pagoConTarjeta;
-  } else {
-    return pagoConTarjeta;
-  }
-}
-
-function descuento10(precioBase, aplicarDescuento) {
-  if (aplicarDescuento) {
-    let descuento;
-    descuento = precioBase * 0.1;
-    return descuento;
-  } else {
-    //NO SE REALIZA DESCUENTO
-    return 0;
-  }
-}
