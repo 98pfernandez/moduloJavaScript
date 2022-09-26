82c33ba215b3c48c3c95b3da11e0f00fa2f67fb8
Pablo Fernandez <pfernandez@fx2.com.uy>
2022-09-26 09:41:39 -0300

    Comentarios en el codigo

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
+const body = document.querySelector("body");
+const carrito = document.querySelector("#bttCarrito");
+
+//Botones de comprar por cada curso
 const buttonJava = document.querySelector("#bttJava");
 const buttonFull = document.querySelector("#bttFullStack");
 const bttPython = document.querySelector("#bttPython");
 
-const cursoComprado = document.querySelector("#cursoComprado");
-const body = document.querySelector("body");
+//Numero de items del carrito
 const nodoCarrito = document.querySelector("#contadorCarrito");
 
+//Obtenemos el session storage con los cursos que tenemos en el carrito. Si no existe lo creamos
 const cursosComprados = JSON.parse(sessionStorage.getItem("curso")) == null ? [] : JSON.parse(sessionStorage.getItem("curso"));
 
-
-
-const carrito = document.querySelector("#bttCarrito");
+//DIV DONDE SE VAN A COLOCAR LOS CURSOS COMPRADOS
+const cursoComprado = document.querySelector("#cursoComprado");
 
 
 body.onload = () => {
-    //CUANDO CARGA EL HTML OBTIENE LA CANTIDAD DE ITEMS QUE HAY EN EL CARRITO. SI ES LA PRIMERA VEZ QUE SE EJECUTA SE CREA EL ITEM
+    //Cuando carga el html obtiene la cantidad de items que hay en el carrito. Si es la primera vez que se ejecuta se setea en 0.
     let numeroDeItemsCarrito = sessionStorage.getItem("cantidadItemCarrito");
 
     if (numeroDeItemsCarrito == null || numeroDeItemsCarrito == '') {
@@ -25,57 +28,61 @@ body.onload = () => {
 }
 
 carrito.onclick = () => {
+    //Cuando clickeamos en el carrito guardamos el array con los cursos comprados y la cantidad de items en el carrito
     sessionStorage.setItem("curso", JSON.stringify(cursosComprados))
-
-    //ALMACENAMOS EN LA SESSION STORAGEe EL NUMERO DE ITEMS DEL CARRITO 
     sessionStorage.setItem("cantidadItemCarrito", nodoCarrito.textContent);
 }
 
 buttonJava.onclick = () => {
-    cursoComprado.style.visibility ="visible";
+    //Mostramos el mensaje 2 segundos
+    cursoComprado.style.visibility = "visible";
     cursoComprado.innerHTML = "<h2>AGREGASTE EL CURSO <FONT COLOR='red'>JAVA</FONT> AL CARRITO</h2> <img src='https://cdn-icons-png.flaticon.com/512/226/226777.png' width='80px' height='80px'>"
-    setTimeout(() => {(cursoComprado.style.visibility = "hidden")}, 2000);
+    setTimeout(() => { (cursoComprado.style.visibility = "hidden") }, 2000);
+
+    //Sumamos item y creamos objeto que se almacenara en el session storage
     sumarItemCarrito(nodoCarrito);
-
     agregarItemCarrito(1, "JAVA", 200, "./JAVA.png")
-
-    console.log(cursosComprados)
 }
 
 
 buttonFull.onclick = () => {
-    cursoComprado.style.visibility ="visible";
+    //Mostramos el mensaje 2 segundos
+    cursoComprado.style.visibility = "visible";
     cursoComprado.innerHTML = "<h2>AGREGASTE  EL CURSO  <FONT COLOR='blue'>FULL STACK</FONT> AL CARRITO</h2> <img src='https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,f_auto,q_auto:eco,dpr_1/v1468442513/ntffgniiqfya5tvzbsol.png' width='80px' height='80px'>"
-    setTimeout(() => {(cursoComprado.style.visibility = "hidden")}, 2000);
+    setTimeout(() => { (cursoComprado.style.visibility = "hidden") }, 2000);
+
+    //Sumamos item y creamos objeto que se almacenara en el session storage
     sumarItemCarrito(nodoCarrito)
     agregarItemCarrito(2, "FULL STACK", 500, "./FULLSTACK.png")
-    console.log(cursosComprados)
 }
 
 bttPython.onclick = () => {
-    cursoComprado.style.visibility ="visible";
+    //Mostramos el mensaje 2 segundos
+    cursoComprado.style.visibility = "visible";
     cursoComprado.innerHTML = "<h2>AGREGASTE EL CURSO <FONT COLOR='yellow'>PYT</FONT><FONT COLOR='blue'>HON</FONT> AL CARRITO</h2> <img src='https://seeklogo.com/images/P/python-logo-C50EED1930-seeklogo.com.png' width='80px' height='80px'>"
-    setTimeout(() => {(cursoComprado.style.visibility = "hidden")}, 2000);
-    
+    setTimeout(() => { (cursoComprado.style.visibility = "hidden") }, 2000);
+
+    //Sumamos item y creamos objeto que se almacenara en el session storage
     sumarItemCarrito(nodoCarrito)
     agregarItemCarrito(3, "PYTHON", 150, "./PYTHON.png")
 }
 
 function agregarItemCarrito(idProducto, nombreProducto, precioProducto, imagenProducto) {
     if (cursosComprados.some(curso => curso.id === idProducto)) {
-        //SI EXISTE EL CURSO CON ID 1, LE SUMAMOS UNO A LA CANTIDAD
+        //Si ya existe el curso con ID=x, para esa posicion le sumamos uno a la cantidad
         cursosComprados.forEach(curso => {
             if (curso.id === idProducto) {
                 curso.cantidad += 1;
             }
         });
     } else {
-        //SI NO EXISTE EL CURSO CON ID 1 LO CREAMOS
+        //Si no existe el curso con ID=x lo creamos y lo pusheamos al Array
         cursosComprados.push(new Producto(idProducto, nombreProducto, precioProducto, imagenProducto))
     }
 }
 
 function sumarItemCarrito(carritoNumero) {
+    //Aumenta en 1 la cantidad de items del carrito.
     carritoNumero.innerText = parseInt(carritoNumero.textContent) + 1
 }
 
@@ -84,7 +91,7 @@ class Producto {
         this.id = id;
         this.nombre = nombre;
         this.precio = precio;
-        this.img= img;
+        this.img = img;
         this.cantidad = 1;
     }
 }
--- createCart.js
+++ createCart.js
@@ -1,8 +1,8 @@
 const body = document.querySelector("body");
 const listaCarrito = document.querySelector("#listaCarrito");
 
-//CUANDO CARGA LA PAGINA SE GENERA EL CARRITO
-body.onload= () => {
+//Cuando carga la pagina se genera la lista del carrito.
+body.onload = () => {
     sessionStorage.getItem("cantidadItemCarrito");
     const nodoCarrito = document.querySelector("#contadorCarrito");
 
@@ -10,21 +10,21 @@ body.onload= () => {
 
     const productos = JSON.parse(sessionStorage.getItem("curso"));
 
-    let divProductos='';
-    let precioTotal=0;
-    let cantidadTotal=0;
+    let divProductos = '';
+    let precioTotal = 0;
+    let cantidadTotal = 0;
     productos.forEach(element => {
         console.log(productos)
-        precioTotal+=(element.precio*element.cantidad);
-        cantidadTotal+=element.cantidad;
-        divProductos+="<div class='fila'><div class='imagenCarrito'><img class='imgCurso' src="+element.img+"></div><div class='nombreCarrito'>"+element.nombre +"</div>  <div class='cantidadCarrito'>"+ element.cantidad+"</div> <div class='precioCarrito'>"+element.precio+ " USD"+"</div> <div class='totalCarrito'>"+(element.precio*element.cantidad) +" USD"+"</div></div>";
+        precioTotal += (element.precio * element.cantidad);
+        cantidadTotal += element.cantidad;
+        divProductos += "<div class='fila'><div class='imagenCarrito'><img class='imgCurso' src=" + element.img + "></div><div class='nombreCarrito'>" + element.nombre + "</div>  <div class='cantidadCarrito'>" + element.cantidad + "</div> <div class='precioCarrito'>" + element.precio + " USD" + "</div> <div class='totalCarrito'>" + (element.precio * element.cantidad) + " USD" + "</div></div>";
 
     });
-    //AGREGAMOS TOTALES DE CANTIDAD Y EFECTIVO
-    divProductos+="<div class='fila'><div class='imagenCarrito'><img style=width:200px src='./Transparent.gif'></div><div class='nombreCarrito'></div>  <div class='cantidadCarrito'>"+cantidadTotal+"</div> <div class='precioCarrito'></div> <div class='totalCarrito'>"+precioTotal +" USD"+"</div></div>";
-   
-   //AGREGAMOS BOTON
-   divProductos+="<div><button id='botonComprar'>Comprar</button></div>"
-    listaCarrito.innerHTML+=divProductos;
 
+    //Totales de cantidad y monto
+    divProductos += "<div class='fila'><div class='imagenCarrito'><img style=width:200px src='./Transparent.gif'></div><div class='nombreCarrito'></div>  <div class='cantidadCarrito'>" + cantidadTotal + "</div> <div class='precioCarrito'></div> <div class='totalCarrito'>" + precioTotal + " USD" + "</div></div>";
+
+    //Agregamos boton al final de la lista
+    divProductos += "<div><button id='botonComprar'>Comprar</button></div>"
+    listaCarrito.innerHTML += divProductos;
 }
